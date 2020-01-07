fe6217704ba102c3a054ba387b9e9a7ca00207ed
Eleanor Hayes-Larson <ehayeslarson@gmail.com>
2020-01-06 17:22:21 -0800

    First commit of code that produces basic description of sample and relative risk regression models

---
+++ HRQoL main analysis.R
@@ -0,0 +1,60 @@
+#------------------------------------------------------------------
+# Title: HRQoL main analysis
+# Author: Eleanor Hayes-Larson
+#------------------------------------------------------------------
+
+#------------------------------------------------------------------
+# Loading packages, options and initializations #
+#------------------------------------------------------------------
+if (!require("pacman")) 
+  install.packages("pacman", repos='http://cran.us.r-project.org')
+
+p_load("haven", "tidyverse", "magrittr", "foreign", "ggplot2", "survey", "tableone")
+
+#------------------------------------------------------------------
+# Load clean data#
+#------------------------------------------------------------------
+load("C:/Users/ehlarson/Box/NHATS/DATA/analysis_datasets/QOL_DEM_analysis_clean.RData")
+
+#Subset to complete cases with prob/poss dementia for all HRQoL vars
+#Limit to black, white, hispanic due to sample size (can add "other" later when pooling)
+clean_data_hrqol<-clean_data[clean_data$comp.case.HRQoL==1 & clean_data$dementia.bin==1,]
+
+#Input complex survey design for analysis
+nhats_design<-svydesign(data=clean_data_hrqol, id=~w5varunit, strata=~w5varstrat, weights=~w5anfinwgt0, nest=T)
+
+
+#Sample descriptives
+
+    #unweighted
+      catvars<-c("age.cat", "female", "race.eth")
+      CreateTableOne(vars=catvars, data=clean_data_hrqol, factorVars=catvars)
+
+    #weighted
+      svymean(~female, nhats_design)
+      svymean(~as.factor(race.eth), nhats_design)
+      svymean(~as.factor(age.cat), nhats_design)
+      svymean(~prob.dep, nhats_design)
+      svymean(~prob.anx, nhats_design)
+      # svymean(~poorhealth.bin, nhats_design)
+      # svymean(~pain.bother, nhats_design)
+      # svymean(~funclimits, nhats_design)
+      # 
+
+#Relative risk regression models
+outcomes<-c("prob.dep", "prob.anx", "poorhealth.bin", "pain.bother", "funclimits")
+models<-list(rep(NA,5))
+results<-data.frame(outcome=outcomes,black_RR=rep(NA,5), black_lci=rep(NA,5), black_uci=rep(NA,5), hispanic_RR=rep(NA,5), hispanic_lci=rep(NA,5), hispanic_uci=rep(NA,5))
+
+for (i in 1:length(outcomes)){
+outcome=colnames(clean_data_hrqol)[i+7]
+model<-svyglm(as.formula(paste(outcome,"~as.factor(race.eth)+as.factor(age.cat)+female")), design=nhats_design, family=quasibinomial(link=log), start=c(-0.5,rep(0,9)))
+results$black_RR[i]<-exp(coef(model)[2])
+results$hispanic_RR[i]<-exp(coef(model)[3])
+results$black_lci[i]<-exp(confint(model)[2,1])
+results$black_uci[i]<-exp(confint(model)[2,2])
+results$hispanic_lci[i]<-exp(confint(model)[3,1])
+results$hispanic_uci[i]<-exp(confint(model)[3,2])
+}
+
+results_t = setNames(data.frame(t(results[,-1])), results[,1])
