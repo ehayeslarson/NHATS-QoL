5b233574ba6463aa129328de58fa35a219c7fe57
Eleanor Hayes-Larson <ehayeslarson@gmail.com>
2020-01-09 13:36:45 -0800

    Added analysis among dementia = 0 sample, and export of results

--- HRQoL main analysis.R
+++ HRQoL main analysis.R
@@ -9,12 +9,16 @@
 if (!require("pacman")) 
   install.packages("pacman", repos='http://cran.us.r-project.org')
 
-p_load("haven", "tidyverse", "magrittr", "foreign", "ggplot2", "survey", "tableone")
+p_load("haven", "tidyverse", "magrittr", "foreign", "ggplot2", "survey", "tableone", "openxlsx")
 
 #------------------------------------------------------------------
 # Load clean data#
 #------------------------------------------------------------------
 load("C:/Users/ehlarson/Box/NHATS/DATA/analysis_datasets/QOL_DEM_analysis_clean.RData")
+
+#------------------------------------------------------------------
+# Analysis among dementa = 1 #
+#------------------------------------------------------------------
 
 #Subset to complete cases with prob/poss dementia for all HRQoL vars
 #Limit to black, white, hispanic due to sample size (can add "other" later when pooling)
@@ -53,7 +57,7 @@ results_unweighted<-data.frame(outcome=outcomes,black_RR=rep(NA,5), black_lci=re
 
 #weighted
     for (i in 1:length(outcomes)){
-    outcome=colnames(clean_data_hrqol)[i+7]
+    outcome=colnames(clean_data_hrqol)[i+8]
     model<-svyglm(as.formula(paste(outcome,"~as.factor(race.eth)+as.factor(age.cat)+female")), design=nhats_design, family=quasibinomial(link=log), start=c(-0.5,rep(0,9)))
     results_weighted$black_RR[i]<-exp(coef(model)[2])
     results_weighted$hispanic_RR[i]<-exp(coef(model)[3])
@@ -68,7 +72,7 @@ results_unweighted<-data.frame(outcome=outcomes,black_RR=rep(NA,5), black_lci=re
 
 #unweighted
 for (i in 1:length(outcomes)){
-  outcome=colnames(clean_data_hrqol)[i+7]
+  outcome=colnames(clean_data_hrqol)[i+8]
   model<-glm(as.formula(paste(outcome,"~as.factor(race.eth)+as.factor(age.cat)+female")), data=clean_data_hrqol, family=binomial(link=log))
   results_unweighted$black_RR[i]<-exp(coef(model)[2])
   results_unweighted$hispanic_RR[i]<-exp(coef(model)[3])
@@ -81,3 +85,76 @@ for (i in 1:length(outcomes)){
   results_unweighted$other_uci[i]<-exp(confint(model)[4,2])
 }
 
+#------------------------------------------------------------------
+# Analysis among dementa = 0 #
+#------------------------------------------------------------------
+#Subset to complete cases with prob/poss dementia for all HRQoL vars
+#Limit to black, white, hispanic due to sample size (can add "other" later when pooling)
+clean_data_hrqol_nodem<-clean_data[clean_data$comp.case.HRQoL==1 & clean_data$dementia.bin==0,]
+
+#Input complex survey design for analysis
+nhats_design_nodem<-svydesign(data=clean_data_hrqol_nodem, id=~w5varunit, strata=~w5varstrat, weights=~w5anfinwgt0, nest=T)
+
+
+#Sample descriptives
+
+#unweighted
+CreateTableOne(vars=catvars, data=clean_data_hrqol_nodem, factorVars=catvars)
+
+#weighted
+svymean(~female, nhats_design_nodem)
+svymean(~as.factor(race.eth), nhats_design_nodem)
+svymean(~as.factor(age.cat), nhats_design_nodem)
+svymean(~prob.dep, nhats_design_nodem)
+svymean(~prob.anx, nhats_design_nodem)
+svymean(~poorhealth.bin, nhats_design_nodem)
+svymean(~pain.bother, nhats_design_nodem)
+svymean(~funclimits, nhats_design_nodem)
+
+
+#Check differences in proxy by race
+CreateTableOne(vars="proxy", strata="race.eth", data=clean_data_hrqol, factorVars=c("race.eth","proxy"))
+
+
+#Relative risk regression models
+
+outcomes<-c("prob.dep", "prob.anx", "poorhealth.bin", "pain.bother", "funclimits")
+results_weighted_nodem<-data.frame(outcome=outcomes,black_RR=rep(NA,5), black_lci=rep(NA,5), black_uci=rep(NA,5), hispanic_RR=rep(NA,5), hispanic_lci=rep(NA,5), hispanic_uci=rep(NA,5), other_RR=rep(NA,5), other_lci=rep(NA,5), other_uci=rep(NA,5))
+results_unweighted_nodem<-data.frame(outcome=outcomes,black_RR=rep(NA,5), black_lci=rep(NA,5), black_uci=rep(NA,5), hispanic_RR=rep(NA,5), hispanic_lci=rep(NA,5), hispanic_uci=rep(NA,5), other_RR=rep(NA,5), other_lci=rep(NA,5), other_uci=rep(NA,5))
+
+#weighted
+for (i in 1:length(outcomes)){
+  outcome=colnames(clean_data_hrqol)[i+8]
+  model<-svyglm(as.formula(paste(outcome,"~as.factor(race.eth)+as.factor(age.cat)+female")), design=nhats_design_nodem, family=quasibinomial(link=log), start=c(-0.5,rep(0,9)))
+  results_weighted_nodem$black_RR[i]<-exp(coef(model)[2])
+  results_weighted_nodem$hispanic_RR[i]<-exp(coef(model)[3])
+  results_weighted_nodem$other_RR[i]<-exp(coef(model)[4])
+  results_weighted_nodem$black_lci[i]<-exp(confint(model)[2,1])
+  results_weighted_nodem$black_uci[i]<-exp(confint(model)[2,2])
+  results_weighted_nodem$hispanic_lci[i]<-exp(confint(model)[3,1])
+  results_weighted_nodem$hispanic_uci[i]<-exp(confint(model)[3,2])
+  results_weighted_nodem$other_lci[i]<-exp(confint(model)[4,1])
+  results_weighted_nodem$other_uci[i]<-exp(confint(model)[4,2])
+}
+
+#unweighted
+for (i in 1:length(outcomes)){
+  outcome=colnames(clean_data_hrqol)[i+8]
+  model<-glm(as.formula(paste(outcome,"~as.factor(race.eth)+as.factor(age.cat)+female")), data=clean_data_hrqol_nodem, family=binomial(link=log))
+  results_unweighted_nodem$black_RR[i]<-exp(coef(model)[2])
+  results_unweighted_nodem$hispanic_RR[i]<-exp(coef(model)[3])
+  results_unweighted_nodem$other_RR[i]<-exp(coef(model)[4])
+  results_unweighted_nodem$black_lci[i]<-exp(confint(model)[2,1])
+  results_unweighted_nodem$black_uci[i]<-exp(confint(model)[2,2])
+  results_unweighted_nodem$hispanic_lci[i]<-exp(confint(model)[3,1])
+  results_unweighted_nodem$hispanic_uci[i]<-exp(confint(model)[3,2])
+  results_unweighted_nodem$other_lci[i]<-exp(confint(model)[4,1])
+  results_unweighted_nodem$other_uci[i]<-exp(confint(model)[4,2])
+}
+
+#------------------------------------------------------------------
+# Export results #
+#------------------------------------------------------------------
+
+results.list <- list("Unweighted_dem" = results_unweighted, "Unweighted_nodem" = results_unweighted_nodem, "Weighted_dem" = results_weighted, "Weighted_nodem" = results_weighted_nodem)
+write.xlsx(results.list, file = "C:/Users/ehlarson/Box/NHATS/OUTPUT/RR_HRQOL_bydem.xlsx")
