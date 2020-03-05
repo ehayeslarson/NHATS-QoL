99301cb51c9f110fae98f081cf52479567598b70
Eleanor Hayes-Larson <ehayeslarson@gmail.com>
2020-03-04 17:48:01 -0800

    Revised code to just do analytic models. descriptive analysis now in separate script.

--- HRQoL main analysis_pooled.R
+++ HRQoL main analysis_pooled.R
@@ -1,5 +1,5 @@
 #------------------------------------------------------------------
-# Title: HRQoL main analysis
+# Title: HRQoL main analysis pooled
 # Author: Eleanor Hayes-Larson
 #------------------------------------------------------------------
 
@@ -9,148 +9,92 @@
 if (!require("pacman")) 
   install.packages("pacman", repos='http://cran.us.r-project.org')
 
-p_load("haven", "tidyverse", "magrittr", "foreign", "ggplot2", "survey", "tableone", "openxlsx")
+p_load("haven", "tidyverse", "magrittr", "foreign", "ggplot2", "gee", "geepack",  
+       "survey", "tableone", "openxlsx")
 
 #------------------------------------------------------------------
 # Load clean data#
 #------------------------------------------------------------------
-load("C:/Users/ehlarson/Box/NHATS/DATA/analysis_datasets/QOL_DEM_analysis_clean.RData")
-
-#------------------------------------------------------------------
-# Analysis among dementa = 1 #
-#------------------------------------------------------------------
-
-#Subset to complete cases with prob/poss dementia for all HRQoL vars
-#Limit to black, white, hispanic due to sample size (can add "other" later when pooling)
-clean_data_hrqol<-clean_data[clean_data$comp.case.HRQoL==1 & clean_data$dementia.bin==1,]
-
-#Input complex survey design for analysis
-nhats_design<-svydesign(data=clean_data_hrqol, id=~w5varunit, strata=~w5varstrat, weights=~w5anfinwgt0, nest=T)
-
-
-#Sample descriptives
-
-    #unweighted
-      catvars<-c("age.cat", "female", "race.eth", "prob.dep", "prob.anx", "poorhealth.bin", "pain.bother", "funclimits")
-      CreateTableOne(vars=catvars, data=clean_data_hrqol, factorVars=catvars)
-
-    #weighted
-      svymean(~female, nhats_design)
-      svymean(~as.factor(race.eth), nhats_design)
-      svymean(~as.factor(age.cat), nhats_design)
-      svymean(~prob.dep, nhats_design)
-      svymean(~prob.anx, nhats_design)
-      svymean(~poorhealth.bin, nhats_design)
-      svymean(~pain.bother, nhats_design)
-      svymean(~funclimits, nhats_design)
-
-      
-#Check differences in proxy by race
-      CreateTableOne(vars="proxy", strata="race.eth", data=clean_data_hrqol, factorVars=c("race.eth","proxy"))
-     
-
-#Relative risk regression models
-      
-outcomes<-c("prob.dep", "prob.anx", "poorhealth.bin", "pain.bother", "funclimits")
-results_weighted<-data.frame(outcome=outcomes,black_RR=rep(NA,5), black_lci=rep(NA,5), black_uci=rep(NA,5), hispanic_RR=rep(NA,5), hispanic_lci=rep(NA,5), hispanic_uci=rep(NA,5), other_RR=rep(NA,5), other_lci=rep(NA,5), other_uci=rep(NA,5))
-results_unweighted<-data.frame(outcome=outcomes,black_RR=rep(NA,5), black_lci=rep(NA,5), black_uci=rep(NA,5), hispanic_RR=rep(NA,5), hispanic_lci=rep(NA,5), hispanic_uci=rep(NA,5), other_RR=rep(NA,5), other_lci=rep(NA,5), other_uci=rep(NA,5))
-
-#weighted
-    for (i in 1:length(outcomes)){
-    outcome=colnames(clean_data_hrqol)[i+8]
-    model<-svyglm(as.formula(paste(outcome,"~as.factor(race.eth)+as.factor(age.cat)+female")), design=nhats_design, family=quasibinomial(link=log), start=c(-0.5,rep(0,9)))
-    results_weighted$black_RR[i]<-exp(coef(model)[2])
-    results_weighted$hispanic_RR[i]<-exp(coef(model)[3])
-    results_weighted$other_RR[i]<-exp(coef(model)[4])
-    results_weighted$black_lci[i]<-exp(confint(model)[2,1])
-    results_weighted$black_uci[i]<-exp(confint(model)[2,2])
-    results_weighted$hispanic_lci[i]<-exp(confint(model)[3,1])
-    results_weighted$hispanic_uci[i]<-exp(confint(model)[3,2])
-    results_weighted$other_lci[i]<-exp(confint(model)[4,1])
-    results_weighted$other_uci[i]<-exp(confint(model)[4,2])
-    }
-
-#unweighted
-for (i in 1:length(outcomes)){
-  outcome=colnames(clean_data_hrqol)[i+8]
-  model<-glm(as.formula(paste(outcome,"~as.factor(race.eth)+as.factor(age.cat)+female")), data=clean_data_hrqol, family=binomial(link=log))
-  results_unweighted$black_RR[i]<-exp(coef(model)[2])
-  results_unweighted$hispanic_RR[i]<-exp(coef(model)[3])
-  results_unweighted$other_RR[i]<-exp(coef(model)[4])
-  results_unweighted$black_lci[i]<-exp(confint(model)[2,1])
-  results_unweighted$black_uci[i]<-exp(confint(model)[2,2])
-  results_unweighted$hispanic_lci[i]<-exp(confint(model)[3,1])
-  results_unweighted$hispanic_uci[i]<-exp(confint(model)[3,2])
-  results_unweighted$other_lci[i]<-exp(confint(model)[4,1])
-  results_unweighted$other_uci[i]<-exp(confint(model)[4,2])
-}
-
-#------------------------------------------------------------------
-# Analysis among dementa = 0 #
-#------------------------------------------------------------------
-#Subset to complete cases with prob/poss dementia for all HRQoL vars
-#Limit to black, white, hispanic due to sample size (can add "other" later when pooling)
+load("C:/Users/ehlarson/Box/NHATS/DATA/analysis_datasets/QOL_DEM_analysis_clean_pooled.RData")
+clean_data_hrqol<-clean_data[clean_data$comp.case.HRQoL==1,]
+clean_data_hrqol_dem<-clean_data[clean_data$comp.case.HRQoL==1 & clean_data$dementia.bin==1,]
 clean_data_hrqol_nodem<-clean_data[clean_data$comp.case.HRQoL==1 & clean_data$dementia.bin==0,]
+outcomes<-c("prob.dep", "prob.anx", "poorhealth.bin", "pain.bother", "funclimits")
 
-#Input complex survey design for analysis
-nhats_design_nodem<-svydesign(data=clean_data_hrqol_nodem, id=~w5varunit, strata=~w5varstrat, weights=~w5anfinwgt0, nest=T)
-
-
-#Sample descriptives
-
-#unweighted
-CreateTableOne(vars=catvars, data=clean_data_hrqol_nodem, factorVars=catvars)
-
-#weighted
-svymean(~female, nhats_design_nodem)
-svymean(~as.factor(race.eth), nhats_design_nodem)
-svymean(~as.factor(age.cat), nhats_design_nodem)
-svymean(~prob.dep, nhats_design_nodem)
-svymean(~prob.anx, nhats_design_nodem)
-svymean(~poorhealth.bin, nhats_design_nodem)
-svymean(~pain.bother, nhats_design_nodem)
-svymean(~funclimits, nhats_design_nodem)
-
-
-#Check differences in proxy by race
-CreateTableOne(vars="proxy", strata="race.eth", data=clean_data_hrqol, factorVars=c("race.eth","proxy"))
-
+# ---Unweighted analyses--- #
 
 #Relative risk regression models
 
-outcomes<-c("prob.dep", "prob.anx", "poorhealth.bin", "pain.bother", "funclimits")
-results_weighted_nodem<-data.frame(outcome=outcomes,black_RR=rep(NA,5), black_lci=rep(NA,5), black_uci=rep(NA,5), hispanic_RR=rep(NA,5), hispanic_lci=rep(NA,5), hispanic_uci=rep(NA,5), other_RR=rep(NA,5), other_lci=rep(NA,5), other_uci=rep(NA,5))
-results_unweighted_nodem<-data.frame(outcome=outcomes,black_RR=rep(NA,5), black_lci=rep(NA,5), black_uci=rep(NA,5), hispanic_RR=rep(NA,5), hispanic_lci=rep(NA,5), hispanic_uci=rep(NA,5), other_RR=rep(NA,5), other_lci=rep(NA,5), other_uci=rep(NA,5))
 
-#weighted
-for (i in 1:length(outcomes)){
-  outcome=colnames(clean_data_hrqol)[i+8]
-  model<-svyglm(as.formula(paste(outcome,"~as.factor(race.eth)+as.factor(age.cat)+female")), design=nhats_design_nodem, family=quasibinomial(link=log), start=c(-0.5,rep(0,9)))
-  results_weighted_nodem$black_RR[i]<-exp(coef(model)[2])
-  results_weighted_nodem$hispanic_RR[i]<-exp(coef(model)[3])
-  results_weighted_nodem$other_RR[i]<-exp(coef(model)[4])
-  results_weighted_nodem$black_lci[i]<-exp(confint(model)[2,1])
-  results_weighted_nodem$black_uci[i]<-exp(confint(model)[2,2])
-  results_weighted_nodem$hispanic_lci[i]<-exp(confint(model)[3,1])
-  results_weighted_nodem$hispanic_uci[i]<-exp(confint(model)[3,2])
-  results_weighted_nodem$other_lci[i]<-exp(confint(model)[4,1])
-  results_weighted_nodem$other_uci[i]<-exp(confint(model)[4,2])
+summary(model)
+
+summary(model)$coefficients[2,"Estimate"]
+      
+test<-as.double(coef(model))
+test[2]
+
+unwghtedmod<-function(indata){
+        outresults<-data.frame(outcome=outcomes,
+                               black_RR=rep(NA,5), hispanic_RR=rep(NA,5), other_RR=rep(NA,5), 
+                               black_lci=rep(NA,5), hispanic_lci=rep(NA,5),  other_lci=rep(NA,5),
+                               black_uci=rep(NA,5),  hispanic_uci=rep(NA,5), other_uci=rep(NA,5))
+        
+          for (i in 1:length(outcomes)){
+                        outcome<-outcomes[i]
+            
+            model<-geeglm(as.formula(paste(outcome,"~as.factor(race.eth)+as.factor(age.cat)+female")), 
+                       id=spid, data=indata, corstr="exchangeable", family=binomial(link=log))
+            
+            for (j in 2:4){
+              outresults[i,j] <- exp(coef(model)[j])
+              outresults[i,j+3] <- exp(coef(model)[j]-1.96*summary(model)$coefficients[j,"Std.err"])
+              outresults[i,j+6] <- exp(coef(model)[j]+1.96*summary(model)$coefficients[j,"Std.err"])
+            }
+          }
+        return(outresults)
+      }
+      
+
+      results_unweighted_dem<-unwghtedmod(clean_data_hrqol_dem)      
+      results_unweighted_nodem<-unwghtedmod(clean_data_hrqol_nodem)      
+      
+      
+# ---Weighted analyses--- #
+
+
+
+wghtedmod<-function(dem,weightvar){
+  outresults<-data.frame(outcome=outcomes,
+                         black_RR=rep(NA,5), hispanic_RR=rep(NA,5), other_RR=rep(NA,5), 
+                         black_lci=rep(NA,5), hispanic_lci=rep(NA,5),  other_lci=rep(NA,5),
+                         black_uci=rep(NA,5),  hispanic_uci=rep(NA,5), other_uci=rep(NA,5))
+  
+  for (i in 1:length(outcomes)){
+    outcome<-outcomes[i]
+    
+    model<-geeglm(as.formula(paste(outcome,"~as.factor(race.eth)+as.factor(age.cat)+female")), 
+               id=spid, data=clean_data_hrqol[clean_data_hrqol$dementia.bin==dem,], 
+               corstr="exchangeable", family=poisson(link=log), weights=weightvar[clean_data_hrqol$dementia.bin==dem])
+    
+    for (j in 2:4){
+      outresults[i,j] <- exp(coef(model)[j])
+      outresults[i,j+3] <- exp(coef(model)[j]-1.96*summary(model)$coefficients[j,"Std.err"])
+      outresults[i,j+6] <- exp(coef(model)[j]+1.96*summary(model)$coefficients[j,"Std.err"])
+    }
+  }
+  return(outresults)
 }
 
-#unweighted
-for (i in 1:length(outcomes)){
-  outcome=colnames(clean_data_hrqol)[i+8]
-  model<-glm(as.formula(paste(outcome,"~as.factor(race.eth)+as.factor(age.cat)+female")), data=clean_data_hrqol_nodem, family=binomial(link=log))
-  results_unweighted_nodem$black_RR[i]<-exp(coef(model)[2])
-  results_unweighted_nodem$hispanic_RR[i]<-exp(coef(model)[3])
-  results_unweighted_nodem$other_RR[i]<-exp(coef(model)[4])
-  results_unweighted_nodem$black_lci[i]<-exp(confint(model)[2,1])
-  results_unweighted_nodem$black_uci[i]<-exp(confint(model)[2,2])
-  results_unweighted_nodem$hispanic_lci[i]<-exp(confint(model)[3,1])
-  results_unweighted_nodem$hispanic_uci[i]<-exp(confint(model)[3,2])
-  results_unweighted_nodem$other_lci[i]<-exp(confint(model)[4,1])
-  results_unweighted_nodem$other_uci[i]<-exp(confint(model)[4,2])
-}
+
+results_weighted_bl_dem<-wghtedmod(1, clean_data_hrqol$baseline.anwgt)      
+results_weighted_avg_dem<-wghtedmod(1, clean_data_hrqol$average.anwgt)      
+results_weighted_rd_dem<-wghtedmod(1, clean_data_hrqol$analytic.wgt)      
+
+results_weighted_bl_nodem<-wghtedmod(0, clean_data_hrqol$baseline.anwgt)     
+results_weighted_avg_nodem<-wghtedmod(0, clean_data_hrqol$average.anwgt)     
+results_weighted_rd_nodem<-wghtedmod(0, clean_data_hrqol$analytic.wgt)     
+
+
 
 #------------------------------------------------------------------
 # Export results #
