fe12fc9cb79f0b27432f8343eccd66323551ca9a
Eleanor Hayes-Larson <ehayeslarson@gmail.com>
2020-03-16 13:49:52 -0700

    fixed typo in name of plotting function, added code to save figure files

--- HRQoL descr analysis_pooled.R
+++ HRQoL descr analysis_pooled.R
@@ -10,24 +10,26 @@ if (!require("pacman"))
   install.packages("pacman", repos='http://cran.us.r-project.org')
 
 p_load("haven", "tidyverse", "magrittr", "foreign", "ggplot2", "gee", "wgeesel", 
-       "survey", "tableone", "openxlsx")
+       "survey", "tableone", "openxlsx", "rlang")
 
 #------------------------------------------------------------------
 # Load clean data#
 #------------------------------------------------------------------
 load("C:/Users/ehlarson/Box/NHATS/DATA/analysis_datasets/QOL_DEM_analysis_clean_pooled.RData")
 
+clean_data_hrqol<-clean_data[clean_data$comp.case.HRQoL==1,]
+
 #Keep baseline observation
 clean_data_hrqol_baseline<-clean_data_hrqol[clean_data_hrqol$first.obs==1,]
 
 
-catvars<-c("age.cat", "female","edu.cat", "resid.care", "cens.div", "born.us", 
-           "proxy", "proxy.fam", "sr.highbp","sr.diabetes","sr.stroke", "sr.cancer")
+catvars<-c("age.cat", "female","edu.7cat", "resid.care", "cens.area", "born.us", 
+           "proxy", "proxy.fam", "sr.highbp","sr.diabetes","sr.stroke", "sr.cancer", "dementia.bin")
 catvar_names<-c("Age (years)", "Female", "Education attained", 
-                "Residential setting", "Census region", "Born in US", 
+                "Lives in residential care setting", "Census region", "Born in US", 
                 "Proxy answered survey", "Familiarity of proxy with routine", 
                 "Self-reported high blood pressure","Self-reported diabetes",
-                "Self-reported stroke", "Self-reported cancer")
+                "Self-reported stroke", "Self-reported cancer", "Dementia")
 
 
 
@@ -42,7 +44,7 @@ catvar_names<-c("Age (years)", "Female", "Education attained",
     for (i in 1:length(catvars)){
     tab.to.add<-table(eval(parse_expr(paste0("clean_data_hrqol_baseline$",catvars[i])))
                       ,clean_data_hrqol_baseline$race.eth, exclude=NULL)
-    labs<-as.character(rownames(tab.to.add))
+    labs<-paste(catvars[i],as.character(rownames(tab.to.add)))
     T1results_unweighted<-rbind(T1results_unweighted, c(paste(catvar_names[i]),rep(NA,4)))
     T1results_unweighted<-rbind(T1results_unweighted,cbind(labs, tab.to.add))
     }
@@ -69,7 +71,7 @@ catvar_names<-c("Age (years)", "Female", "Education attained",
   for (i in 1:length(catvars)){
     tab.to.add<-svytable(~eval(parse_expr(paste0("clean_data_hrqol_baseline$",catvars[i])))+race.eth, 
                          nhats_design, exclude=NULL, round=T, na.action=na.pass, addNA=TRUE)
-    labs<-as.character(rownames(tab.to.add))
+    labs<-paste(catvars[i],as.character(rownames(tab.to.add)))
     T1results_weighted<-rbind(T1results_weighted, c(paste(catvar_names[i]),rep(NA,4)))
     T1results_weighted<-rbind(T1results_weighted,cbind(labs, tab.to.add))
   }
@@ -77,10 +79,18 @@ catvar_names<-c("Age (years)", "Female", "Education attained",
   colnames(T1results_weighted)<-c("Variable name", "Non-Latino white", "Black", "Latino", "Other")
   rownames(T1results_weighted)<-NULL
   T1results_weighted[is.na(T1results_weighted[,1]),"Variable name"]<-"Missing"
-
+  
 #------------------------------------------------------------------
 # Save tables #
 #------------------------------------------------------------------
   T1.list <- list("Unweighted" = T1results_unweighted, "Weighted" = T1results_weighted)
   write.xlsx(T1.list, file = "C:/Users/ehlarson/Box/NHATS/OUTPUT/Table1.xlsx")
+  
+  
+  
+#------------------------------------------------------------------
+# Prevalence of outcomes #
+#------------------------------------------------------------------
+
+  
   
